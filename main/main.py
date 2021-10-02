419684d6f023437413d7a4636548d66c3bff4d42
Siddharth Chopra <contact.sid.chopra@gmail.com>
2021-10-02 16:52:46 +0530

    Bug Fixes

--- main/main.py
+++ main/main.py
@@ -14,8 +14,8 @@ WHITE = (255, 255, 255)
 
 
 def resource_path(relative_path):
-    #base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    #return os.path.join(base_path, relative_path)
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
     return relative_path
 
 
